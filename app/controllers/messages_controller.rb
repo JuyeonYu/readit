f387b376241968f9534a0236a2c356dc47a26cfd
juyeon <remake867@gmail.com>
2026-01-30 22:24:31 +0900

    Build message form with validation feedback

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,18 @@
 class MessagesController < ApplicationController
   def new
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(message_params)
+    @message.valid? # Trigger validation
+    # T04에서 저장 로직 구현 예정
+    render :new, status: :unprocessable_entity
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
