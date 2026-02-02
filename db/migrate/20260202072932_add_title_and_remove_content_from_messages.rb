57f8dcf829fed5cdccf1b2b32f6a34e376d3df8a
juyeon <remake867@gmail.com>
2026-02-02 22:22:34 +0900

    Backfill message titles before dropping content

--- db/migrate/20260202072932_add_title_and_remove_content_from_messages.rb
+++ db/migrate/20260202072932_add_title_and_remove_content_from_messages.rb
@@ -1,6 +1,23 @@
 class AddTitleAndRemoveContentFromMessages < ActiveRecord::Migration[8.1]
-  def change
-    add_column :messages, :title, :string, null: false
+  def up
+    # First add column allowing NULL
+    add_column :messages, :title, :string
+
+    # Set default title for existing records
+    Message.reset_column_information
+    Message.find_each do |message|
+      message.update_column(:title, "Untitled Message")
+    end
+
+    # Now make it NOT NULL
+    change_column_null :messages, :title, false
+
+    # Remove old content column
     remove_column :messages, :content, :text
+  end
+
+  def down
+    add_column :messages, :content, :text
+    remove_column :messages, :title
   end
 end
